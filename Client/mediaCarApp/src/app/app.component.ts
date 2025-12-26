3767690bc1012b95a63a9ee15f03a899a3c7768f
allilova <80039522+allilova@users.noreply.github.com>
2025-12-26 22:51:18 +0200

    update media-catalog

--- Client/mediaCarApp/src/app/app.component.ts
+++ Client/mediaCarApp/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import {NavBarComponent} from "./nav-bar/nav-bar.component";
 import {ServicesCatalogComponent} from "./services-catalog/services-catalog.component";
 import { ContactComponent } from './contact/contact.component';
+import { MediaCatalogComponent } from './media-catalog/media-catalog.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavBarComponent, ServicesCatalogComponent, ContactComponent],
+  imports: [NavBarComponent, ServicesCatalogComponent, ContactComponent, MediaCatalogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Client/mediaCarApp/src/app/media-catalog/media-catalog.component.ts
+++ Client/mediaCarApp/src/app/media-catalog/media-catalog.component.ts
@@ -8,4 +8,49 @@ import { Component } from '@angular/core';
 })
 export class MediaCatalogComponent {
 
+  itemsPerPage = 6;
+  currentPage = 1;
+
+  
+  products = [
+    { title: 'VW Passat B6 B7 2010-2015', price: '270 лв', img: 'mediaExample.PNG' },
+    { title: 'Audi A4 B8 2008-2016', price: '320 лв', img: 'mediaExample.PNG' },
+    { title: 'BMW E46 Android 12', price: '290 лв', img: 'mediaExample.PNG' },
+    { title: 'Mercedes W203 C-Class', price: '350 лв', img: 'mediaExample.PNG' },
+    { title: 'Toyota Corolla 2007', price: '250 лв', img: 'mediaExample.PNG' },
+    { title: 'Honda Civic 8th Gen', price: '280 лв', img: 'mediaExample.PNG' },
+    { title: 'Opel Astra H', price: '260 лв', img: 'mediaExample.PNG' }, 
+    { title: 'Golf 5 / 6 Navigation', price: '275 лв', img: 'mediaExample.PNG' },
+  ];
+
+
+  get visibleProducts() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return this.products.slice(startIndex, endIndex);
+  }
+
+ 
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.scrollToTop();
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.scrollToTop();
+    }
+  }
+
+  get totalPages() {
+    return Math.ceil(this.products.length / this.itemsPerPage);
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
+
