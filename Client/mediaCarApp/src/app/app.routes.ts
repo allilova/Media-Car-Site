87fef9bae8df7250a2b258c4307d9db78bc0df66
allilova <80039522+allilova@users.noreply.github.com>
2025-12-27 23:20:44 +0200

    create search

--- Client/mediaCarApp/src/app/app.routes.ts
+++ Client/mediaCarApp/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { BasketShopComponent } from './basket-shop/basket-shop.component';
 import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
 
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
-    {path: 'basket-shop', component: BasketShopComponent}
+    {path: 'basket-shop', component: BasketShopComponent},
+    {path: 'search', component: SearchComponent}
 ];
